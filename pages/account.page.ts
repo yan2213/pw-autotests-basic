c75be6f0806421a8720689d6f7a803271eaae5dd
yan2213 <zakharchenko2213@gmail.com>
2026-06-11 22:13:54 +0300

    refactor: use shorter way for property declaration in constructor

--- pages/account.page.ts
+++ pages/account.page.ts
@@ -2,12 +2,10 @@ import { Page, Locator } from '@playwright/test';
 import { AuthenticatedHeaderFragment } from './fragments/loggedin-user-header.fragment';
 
 export class AccountPage{
-    page: Page;
     pageTitle: Locator;
     header: AuthenticatedHeaderFragment;
 
-  constructor(page: Page) {
-    this.page = page;
+  constructor(public page: Page) {
     this.pageTitle = this.page.getByTestId("page-title");
     this.header = new AuthenticatedHeaderFragment(page);
   }
